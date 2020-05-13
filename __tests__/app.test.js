2cd7956f9d63a2969bde8964a6419d5c7b8b233f
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-13 14:08:30 -0700

    route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,41 @@
+require('dotenv').config();
+
+const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('tarot_backend routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    mongoose.connection.close();
+  });
+
+  it('creates a card via POST', () => {
+    return request(app)
+      .post('/api/v1/cards')
+      .send({
+      name: 'Empress',
+      number: 3,
+      image: 'URL',
+      description: 'Dope Queen'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+        _id: expect.any(String),
+        name: 'Empress',
+        number: 3,
+        image: 'URL',
+        description: 'Dope Queen',
+        __v: 0
+        });
+      });
   });
 });
